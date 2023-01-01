ea9bae9abce0a228904a8c2fc648bd564a43f28c
Nidhish Rai <92910390+Nidhish2002@users.noreply.github.com>
2023-01-01 18:37:10 +0530

    second commit

--- hotel.sql
+++ hotel.sql
@@ -1,50 +1,13 @@
--- phpMyAdmin SQL Dump
--- version 4.9.0.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: May 25, 2020 at 03:37 PM
--- Server version: 10.4.6-MariaDB
--- PHP Version: 7.3.9
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `hotel`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `admin`
---
-
 CREATE TABLE `admin` (
   `name` varchar(20) NOT NULL,
   `pass` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `admin`
---
-
-INSERT INTO `admin` (`name`, `pass`) VALUES
-('admin', 'admin');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `bookingstatus`
---
 
 CREATE TABLE `bookingstatus` (
   `email` varchar(40) NOT NULL,
@@ -55,18 +18,6 @@ CREATE TABLE `bookingstatus` (
   `date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `bookingstatus`
---
-
-INSERT INTO `bookingstatus` (`email`, `category`, `type`, `roomWant`, `status`, `date`) VALUES
-('iamsabit99@gmail.com', 'Business Class ', 'Single Bed', 21, 0, '2020-05-03');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `category`
---
 
 CREATE TABLE `category` (
   `name` varchar(20) NOT NULL,
@@ -77,22 +28,6 @@ CREATE TABLE `category` (
   `dec` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `category`
---
-
-INSERT INTO `category` (`name`, `type`, `cost`, `available`, `img`, `dec`) VALUES
-('Business Class ', 'Double Bed', 1200, 11, '/assets/img/rooms/room1.jpg', 'Non AC Room'),
-('Business Class ', 'Double Bed', 2005, 9, '/assets/img/rooms/room2.jpg', 'AC Room'),
-('Business Class ', 'Single Bed', 800, 10, '/assets/img/rooms/room3.jpg', 'Non AC Room'),
-('Business Class ', 'Single Bed', 1200, 10, '/assets/img/rooms/room4.jpg', 'AC Room'),
-('First class', 'Double Bed', 1357, 24, '/assets/img/rooms/First classDouble Bed1357.png', 'This is a good room');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
---
 
 CREATE TABLE `user` (
   `name` varchar(20) NOT NULL,
@@ -101,61 +36,12 @@ CREATE TABLE `user` (
   `password` varchar(300) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`name`, `email`, `phone`, `password`) VALUES
-('admin', 'admin@admin.com', '1', '1'),
-('Sabit', 'iamsabit99@gmail.com', '01744248058', '1'),
-('Sabit', 'st.sabit13@gmail.com', '01744248058', '1');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `bookingstatus`
---
-ALTER TABLE `bookingstatus`
-  ADD PRIMARY KEY (`email`,`category`,`type`,`roomWant`,`date`);
-
---
--- Indexes for table `category`
---
-ALTER TABLE `category`
-  ADD PRIMARY KEY (`name`,`type`,`cost`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`email`);
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `bookingstatus`
---
-ALTER TABLE `bookingstatus`
-  ADD CONSTRAINT `fk_01` FOREIGN KEY (`email`) REFERENCES `user` (`email`);
-
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
 CREATE TABLE `backup` (
   `name` varchar(20) NOT NULL,
   `email` varchar(40) NOT NULL,
   `phone` varchar(11) NOT NULL,
   `password` varchar(300) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-ALTER TABLE `backup`
-  ADD PRIMARY KEY (`email`);
 
   CREATE TABLE `backupbooking` (
   `email` varchar(40) NOT NULL,
@@ -166,14 +52,47 @@ ALTER TABLE `backup`
   `date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+
+INSERT INTO `admin` (`name`, `pass`) VALUES
+('admin', 'admin');
+
+INSERT INTO `bookingstatus` (`email`, `category`, `type`, `roomWant`, `status`, `date`) VALUES
+('iamsabit99@gmail.com', 'Business Class ', 'Single Bed', 21, 0, '2020-05-03');
+
+INSERT INTO `category` (`name`, `type`, `cost`, `available`, `img`, `dec`) VALUES
+('Business Class ', 'Double Bed', 1200, 11, '/assets/img/rooms/room1.jpg', 'Non AC Room'),
+('Business Class ', 'Double Bed', 2005, 9, '/assets/img/rooms/room2.jpg', 'AC Room'),
+('Business Class ', 'Single Bed', 800, 10, '/assets/img/rooms/room3.jpg', 'Non AC Room'),
+('Business Class ', 'Single Bed', 1200, 10, '/assets/img/rooms/room4.jpg', 'AC Room'),
+('First class', 'Double Bed', 1357, 24, '/assets/img/rooms/First classDouble Bed1357.png', 'This is a good room');
+
+INSERT INTO `user` (`name`, `email`, `phone`, `password`) VALUES
+('admin', 'admin@admin.com', '1', '1'),
+('Sabit', 'iamsabit99@gmail.com', '01744248058', '1'),
+('Sabit', 'st.sabit13@gmail.com', '01744248058', '1');
+
+ALTER TABLE `bookingstatus`
+  ADD PRIMARY KEY (`email`,`category`,`type`,`roomWant`,`date`);
+
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`name`,`type`,`cost`);
+
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`email`);
+
+ALTER TABLE `bookingstatus`
+  ADD CONSTRAINT `fk_01` FOREIGN KEY (`email`) REFERENCES `user` (`email`);
+
+ALTER TABLE `backup`
+  ADD PRIMARY KEY (`email`);
+
 ALTER TABLE `backupbooking`
   ADD PRIMARY KEY (`email`,`category`,`type`,`roomWant`,`date`);
 
---   CREATE TRIGGER `t1` BEFORE DELETE ON `user`
---  FOR EACH ROW INSERT INTO backup VALUES(old.name,old.email,old.phone,old.password)
+  CREATE TRIGGER `t1` BEFORE DELETE ON `user`
+ FOR EACH ROW INSERT INTO backup VALUES(old.name,old.email,old.phone,old.password)
 
--- CREATE TRIGGER `t2` BEFORE DELETE ON `bookingstatus`
---  FOR EACH ROW INSERT INTO backupbooking VALUES(old.email,old.category,old.type,old.roomWant,old.status,old.date)
-
+CREATE TRIGGER `t2` BEFORE DELETE ON `bookingstatus`
+ FOR EACH ROW INSERT INTO backupbooking VALUES(old.email,old.category,old.type,old.roomWant,old.status,old.date)
 
 COMMIT;
